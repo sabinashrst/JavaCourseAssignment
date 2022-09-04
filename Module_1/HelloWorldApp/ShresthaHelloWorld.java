843e74d3cf406c792c4c2056ac8262c2c27551a8
Sabina <sabshrestha1@my365.bellevue.edu>
2022-09-04 09:37:42 -0500

    First Assignment

---
+++ Module_1/HelloWorldApp/ShresthaHelloWorld.java
@@ -0,0 +1,9 @@
+//This is first assignment
+
+public class ShresthaHelloWorld{
+    public static void main(String args[]){
+        String firstName = "Sabina";
+        String lastName = "Shrestha";
+        System.out.printf("Hello World from " + firstName + " " + lastName);
+    }
+}
