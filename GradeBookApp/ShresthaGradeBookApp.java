4fb8ae5574e13d7ab60875646b8c607b9ff415b3
sakilshrst <sakil.shrestha@gmail.com>
2022-11-15 11:25:57 -0600

    Week 11.2 Assignment

--- GradeBookApp/ShresthaGradeBookApp.java
+++ GradeBookApp/ShresthaGradeBookApp.java
@@ -1,8 +1,17 @@
 //Name: Sabina Shrestha
-//Assignment: 10.2
+//Assignment: 11.2
 
 
 package GradeBookApp;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -12,6 +21,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -30,6 +40,8 @@ public class ShresthaGradeBookApp extends Application {
     private Button btnClear = new Button("Clear");
     private Button btnView = new Button("View");
     private Button btnSave = new Button("Save");
+
+    private TextArea txtResults = new TextArea();
 
 
     public static void main(String[] args) {
@@ -62,11 +74,32 @@ public class ShresthaGradeBookApp extends Application {
         gridPane.add(lblGrade, 0, 3);
         cbGrade.getItems().addAll("A", "B", "C", "D", "E", "F");
         cbGrade.setMaxWidth(Double.MAX_VALUE);
-        gridPane.add(cbGrade, 1,3); 
-
+        gridPane.add(cbGrade, 1,3);
+        
         gridPane.add(btnClear, 0, 4); 
         gridPane.add(btnView, 1, 4);
         gridPane.add(btnSave, 2, 4);
+
+        GridPane.setColumnSpan(txtResults, GridPane.REMAINING);
+        gridPane.add(txtResults, 0, 5); 
+
+        btnSave.setOnAction(e -> {
+            try {
+                writeCSV();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }); 
+
+        btnView.setOnAction(e -> {
+            try {
+                readCSV();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }); 
+
+        btnClear.setOnAction(e -> clearFormFields());
 
         Scene scene = new Scene(gridPane);  
          
@@ -76,5 +109,60 @@ public class ShresthaGradeBookApp extends Application {
         //Displaying the contents of the stage 
         primaryStage.show();      
     }
+
+    private void writeCSV() throws Exception {
+        
+        String firstName = txtFirstName.getText();
+		String lastName = txtLastName.getText();
+		String course = txtCourseName.getText();
+		String grade = cbGrade.getValue();
+        
+        List<String[]> students = new ArrayList<>();
+        students.add(new String [] {firstName,lastName,course,grade});
+
+        //This class allows us to write the data to a CSV file.
+        CSVWriter writer = new CSVWriter(new FileWriter("output.csv"));
+        String[] header="firstName,lastName,course,grade".split(",");
+        writer.writeNext(header);
+        writer.writeAll(students);
+        writer.close();
+    }
+
+
+    private void readCSV() throws Exception {
+        
+        FileReader filereader = new FileReader("output.csv");
+
+        // create csvReader object and skip first Line
+        CSVReader csvReader = new CSVReader(filereader);
+		List<String[]> records = csvReader.readAll();
+
+		Iterator<String[]> iterator = records.iterator();
+
+		while (iterator.hasNext()) {
+			String[] record = iterator.next();
+			Student student = new Student();
+			student.setFirstName(record[0]);
+			student.setLastName(record[1]);
+			student.setCourseName(record[2]);
+            student.setGrade(record[3]);
+            txtResults.setText(student.toString());
+			
+		}
+        // close readers
+        csvReader.close();
+        filereader.close();
+    }
+
+   //This method set all the text values and areas to empty string
+    private void clearFormFields() {
+        
+        txtFirstName.setText("");
+        txtLastName.setText("");
+        txtCourseName.setText("");
+        cbGrade.setValue("");
+        txtResults.setText("");
+    }
     
 }
+
